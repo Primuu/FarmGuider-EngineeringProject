5e5132f43457354f961b4dfe6dbbd78c3c7ac8e4
Adam <trentowski.adam@gmail.com>
2023-10-19 17:38:54 +0200

    Add GlobalExceptionHandler

---
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package pl.edu.uwm.farmguider.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import pl.edu.uwm.farmguider.exceptions.user.UserAlreadyExistsException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+
+    @ExceptionHandler(value = UserAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorMessage userAlreadyExistsException(UserAlreadyExistsException exception) {
+        return new ErrorMessage(exception.getFieldName(), exception.getMessage());
+    }
+
+}
