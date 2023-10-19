615b2e48b880c2bf6333249072de75243fbd8aa0
Adam <trentowski.adam@gmail.com>
2023-10-19 17:39:25 +0200

    Update UserRepository

--- Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/repositories/UserRepository.java
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/repositories/UserRepository.java
@@ -3,5 +3,10 @@ package pl.edu.uwm.farmguider.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.edu.uwm.farmguider.models.user.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    public Optional<User> findUserByEmail(String email);
+
 }
