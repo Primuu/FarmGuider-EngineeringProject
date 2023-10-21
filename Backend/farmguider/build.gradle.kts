09f2aeb45f0372b0ff736f6a2ce3177b4761322b
Adam <trentowski.adam@gmail.com>
2023-10-21 15:05:33 +0200

    Add security and jwt dependencies

--- Backend/farmguider/build.gradle.kts
+++ Backend/farmguider/build.gradle.kts
@@ -23,6 +23,7 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.flywaydb:flyway-core:9.20.1")
@@ -30,9 +31,13 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
 	implementation ("org.hibernate.validator:hibernate-validator:7.0.1.Final")
 	implementation ("jakarta.validation:jakarta.validation-api:3.0.0")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+	implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
 }
 
 tasks.withType<Test> {
