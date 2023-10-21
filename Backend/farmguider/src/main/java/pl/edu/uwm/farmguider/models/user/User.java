e373eb8d46f1bac531db580ea611f53a46270a4b
Adam <trentowski.adam@gmail.com>
2023-10-21 15:17:01 +0200

    Add UserDetails implementation in User

--- Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/models/user/User.java
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/models/user/User.java
@@ -3,8 +3,14 @@ package pl.edu.uwm.farmguider.models.user;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import pl.edu.uwm.farmguider.models.address.Address;
 import pl.edu.uwm.farmguider.models.basic.BasicEntity;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Entity
 @Table(name = "users")
@@ -13,7 +19,7 @@ import pl.edu.uwm.farmguider.models.basic.BasicEntity;
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class User extends BasicEntity {
+public class User extends BasicEntity implements UserDetails {
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "address_id", referencedColumnName = "id")
@@ -23,5 +29,35 @@ public class User extends BasicEntity {
     String password;
     String firstName;
     String lastName;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 }
