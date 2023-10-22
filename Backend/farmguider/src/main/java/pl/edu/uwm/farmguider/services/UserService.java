51f1a2b9767e2f16c897dc7d9cf2464e6fe31667
Adam <trentowski.adam@gmail.com>
2023-10-22 09:54:02 +0200

    Change way of creating user (register), add getUserByEmail method

--- Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/services/UserService.java
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/services/UserService.java
@@ -1,7 +1,9 @@
 package pl.edu.uwm.farmguider.services;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import pl.edu.uwm.farmguider.exceptions.global.EntityNotFoundException;
 import pl.edu.uwm.farmguider.models.address.Address;
 import pl.edu.uwm.farmguider.models.user.User;
 import pl.edu.uwm.farmguider.models.user.dtos.UserCreateDTO;
@@ -12,12 +14,19 @@ import pl.edu.uwm.farmguider.repositories.UserRepository;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new EntityNotFoundException("User", "User with email: " + email +" not found."));
+    }
 
     public User createUser(UserCreateDTO userCreateDTO, Address address) {
+        String encodedPassword = passwordEncoder.encode(userCreateDTO.password());
         User user = new User(
                 address,
                 userCreateDTO.email(),
-                userCreateDTO.password(),
+                encodedPassword,
                 userCreateDTO.firstName(),
                 userCreateDTO.lastName()
         );
@@ -25,7 +34,7 @@ public class UserService {
     }
 
     public boolean userExistsByEmail(String email) {
-        return userRepository.findUserByEmail(email).isPresent();
+        return userRepository.findByEmail(email).isPresent();
     }
 
 }
