62cd6a79bc8b711ba08c703cf25d3173f31a0717
Adam <trentowski.adam@gmail.com>
2023-10-22 14:28:47 +0200

    Update register method in AuthenticationController and add docs

--- Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/controllers/AuthenticationController.java
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/controllers/AuthenticationController.java
@@ -1,35 +1,64 @@
 package pl.edu.uwm.farmguider.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.Cookie;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pl.edu.uwm.farmguider.exceptions.ErrorResponse;
 import pl.edu.uwm.farmguider.facades.UserFacade;
 import pl.edu.uwm.farmguider.models.user.dtos.UserCreateDTO;
 import pl.edu.uwm.farmguider.models.user.dtos.UserResponseDTO;
 import pl.edu.uwm.farmguider.security.AuthenticationRequestDTO;
-import pl.edu.uwm.farmguider.security.services.AuthenticationService;
+import pl.edu.uwm.farmguider.services.AuthenticationService;
+
+import static pl.edu.uwm.farmguider.security.utils.CookieUtils.*;
 
 @RestController
 @RequiredArgsConstructor
-@Tag(name = "Authentication Controller", description = "Functionalities intended for registration and login")
+@Tag(name = "Authentication Controller",
+        description = "Functionalities intended for registration and login.")
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
     private final UserFacade userFacade;
 
+    @Operation(summary = "Register (create) User", description = "Creates a user from the provided payload")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Successful creation of user",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = UserResponseDTO.class)
+                    )),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Bad Request - returns map of errors",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    ))
+    })
     @PostMapping("/register")
-//    public ResponseEntity<UserResponseDTO> register(@RequestBody @Valid UserCreateDTO userCreateDTO) {
-    public ResponseEntity<String> register(@RequestBody @Valid UserCreateDTO userCreateDTO) {
+    public ResponseEntity<UserResponseDTO> register(@RequestBody @Valid UserCreateDTO userCreateDTO) {
         UserResponseDTO userResponseDTO = userFacade.createUser(userCreateDTO);
         String token = authenticationService.authenticate(userCreateDTO.email(), userCreateDTO.password());
-//        return new ResponseEntity<>(userResponseDTO, HttpStatus.CREATED);
-        return new ResponseEntity<>(token, HttpStatus.CREATED);
-//        TODO: set cookie
+        Cookie sessionCookie = createCookie(JWT_COOKIE_NAME, token, COOKIE_MAX_AGE_1_DAY, COOKIE_DEFAULT_PATH);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header(HttpHeaders.SET_COOKIE, formatCookieHeader(sessionCookie))
+                .body(userResponseDTO);
     }
 
     @PostMapping("/authenticate")
