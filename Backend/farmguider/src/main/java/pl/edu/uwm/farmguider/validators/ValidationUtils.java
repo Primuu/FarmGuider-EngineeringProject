57e4fc7b6080fd496410adf04e81ea9babacfe6d
Adam <trentowski.adam@gmail.com>
2023-10-22 14:30:23 +0200

    Update password regex in ValidationUtils

--- Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/validators/ValidationUtils.java
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/validators/ValidationUtils.java
@@ -2,7 +2,8 @@ package pl.edu.uwm.farmguider.validators;
 
 public class ValidationUtils {
 
-    public static final String PASSWORD_REGEX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
+    public static final String PASSWORD_REGEX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()-_=+{}\\[\\]<>?~])" +
+            "[A-Za-z\\d!@#$%^&*()-_=+{}\\[\\]<>?~]{8,}$";
     public static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@(.+\\.)+[A-Za-z]{2,}$";
     public static final String NAME_REGEX = "^[\\p{L}]*$";
 
