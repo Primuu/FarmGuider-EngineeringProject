0fc7f78fcadb2153195493bd44a3b04442d7a113
Adam <trentowski.adam@gmail.com>
2023-10-25 16:47:31 +0200

    Add rest to authenticate

--- Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/controllers/AuthenticationController.java
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/controllers/AuthenticationController.java
@@ -35,7 +35,8 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
     private final UserFacade userFacade;
 
-    @Operation(summary = "Register (create) User", description = "Creates a user from the provided payload")
+    @Operation(summary = "Register (create) User", description = "Creates a user based on the provided payload " +
+            "and logs him in - a cookie with a JWT token containing the email address is set")
     @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "201",
@@ -63,11 +64,32 @@ public class AuthenticationController {
                 .body(userResponseDTO);
     }
 
+    @Operation(summary = "Logs the User in", description = "Logs the user in using the given credentials" +
+            " - a cookie with a JWT token containing the email address is set")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "User logged in successfully",
+                    content = @Content(
+                            mediaType = "text/plain",
+                            schema = @Schema(implementation = String.class)
+                    )),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Bad Request - returns map of errors",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    ))
+    })
     @PostMapping(AUTHENTICATE_URL)
     public ResponseEntity<String> authenticate(@RequestBody AuthenticationRequestDTO request) {
         String token = authenticationService.authenticate(request.email(), request.password());
-        return new ResponseEntity<>(token, HttpStatus.OK);
-        //        TODO: set cookie
+        Cookie sessionCookie = createCookie(JWT_COOKIE_NAME, token, COOKIE_MAX_AGE_1_DAY, COOKIE_DEFAULT_PATH);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header(HttpHeaders.SET_COOKIE, formatCookieHeader(sessionCookie))
+                .body("Successfully logged in");
     }
 
 //    TODO: revoke?
