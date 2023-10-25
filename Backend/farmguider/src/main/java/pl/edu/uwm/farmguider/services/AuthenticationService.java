33110073c57a4283603b643c39dd7781586e237f
Adam <trentowski.adam@gmail.com>
2023-10-25 16:47:00 +0200

    Change BadCredentialsException to custom UnauthorizedException

--- Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/services/AuthenticationService.java
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/services/AuthenticationService.java
@@ -5,6 +5,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
+import pl.edu.uwm.farmguider.exceptions.global.UnauthorizedException;
 import pl.edu.uwm.farmguider.models.user.User;
 import pl.edu.uwm.farmguider.security.utils.JWTUtils;
 
@@ -22,7 +23,7 @@ public class AuthenticationService {
                     new UsernamePasswordAuthenticationToken(email, password)
             );
         } catch (BadCredentialsException exception) {
-            throw new BadCredentialsException("Incorrect email address or password.");
+            throw new UnauthorizedException("Credentials", "Incorrect email address or password.");
         }
         User user = userService.getUserByEmail(email);
         return jwtService.generateToken(user);
