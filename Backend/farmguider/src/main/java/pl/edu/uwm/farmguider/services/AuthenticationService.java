f11071a1b9c14618f716b1037712e54c9eb40e6c
Adam <trentowski.adam@gmail.com>
2023-10-25 18:27:59 +0200

    Add revoke method in AuthenticationService

--- Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/services/AuthenticationService.java
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/services/AuthenticationService.java
@@ -6,7 +6,9 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 import pl.edu.uwm.farmguider.exceptions.global.UnauthorizedException;
+import pl.edu.uwm.farmguider.models.blacklistedToken.BlacklistedToken;
 import pl.edu.uwm.farmguider.models.user.User;
+import pl.edu.uwm.farmguider.repositories.BlacklistedTokenRepository;
 import pl.edu.uwm.farmguider.security.utils.JWTUtils;
 
 @Service
@@ -16,6 +18,7 @@ public class AuthenticationService {
     private final UserService userService;
     private final AuthenticationManager authenticationManager;
     private final JWTUtils jwtService;
+    private final BlacklistedTokenRepository blacklistedTokenRepository;
 
     public String authenticate(String email, String password) {
         try {
@@ -27,6 +30,11 @@ public class AuthenticationService {
         }
         User user = userService.getUserByEmail(email);
         return jwtService.generateToken(user);
+    }
+
+    public void revoke(String token) {
+        BlacklistedToken blacklistedToken = new BlacklistedToken(token);
+        blacklistedTokenRepository.saveAndFlush(blacklistedToken);
     }
 
 }
