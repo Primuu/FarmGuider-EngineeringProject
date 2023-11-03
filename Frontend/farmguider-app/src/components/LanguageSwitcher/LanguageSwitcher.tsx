a28ead182194c6e827bb33566d833e0fc5995a88
Adam <trentowski.adam@gmail.com>
2023-11-03 19:25:49 +0100

    Add flags to LanguageSwitcher

--- Frontend/farmguider-app/src/components/LanguageSwitcher/LanguageSwitcher.tsx
+++ Frontend/farmguider-app/src/components/LanguageSwitcher/LanguageSwitcher.tsx
@@ -3,9 +3,15 @@ import {useTranslation} from 'react-i18next';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Button from '@mui/material/Button';
+import '@/components/LanguageSwitcher/languageSwitcher.css';
+
+import gbFlag from "@/assets/gb.svg"
+import plFlag from "@/assets/pl.svg"
+
+type LangType = 'en' | 'pl';
 
 const LanguageSwitcher = () => {
-    const { i18n } = useTranslation();
+    const {i18n} = useTranslation();
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
     const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
@@ -16,23 +22,41 @@ const LanguageSwitcher = () => {
         setAnchorEl(null);
     };
 
-    const changeLanguage = (lang: 'en' | 'pl') => {
+    const changeLanguage = (lang: LangType) => {
         void i18n.changeLanguage(lang);
         handleClose();
+    };
+
+    const getFlagForLanguage = (lang: LangType) => {
+        switch (lang) {
+            case 'en':
+                return gbFlag;
+            case 'pl':
+                return plFlag;
+            default:
+                return gbFlag;
+        }
     };
 
     return (
         <div>
             <Button onClick={handleClick}>
-                Language
+                <img src={getFlagForLanguage(i18n.language as LangType)} alt="Language flag" className="flag" />
+                     Language
             </Button>
             <Menu
                 anchorEl={anchorEl}
                 open={Boolean(anchorEl)}
                 onClose={handleClose}
             >
-                <MenuItem onClick={() => changeLanguage('en')}>English</MenuItem>
-                <MenuItem onClick={() => changeLanguage('pl')}>Polski</MenuItem>
+                <MenuItem onClick={() => changeLanguage('en')}>
+                    <img src={gbFlag} alt="English Flag" className="flag" />
+                    English
+                </MenuItem>
+                <MenuItem onClick={() => changeLanguage('pl')}>
+                    <img src={plFlag} alt="Polish Flag" className="flag" />
+                    Polski
+                </MenuItem>
             </Menu>
         </div>
     );
