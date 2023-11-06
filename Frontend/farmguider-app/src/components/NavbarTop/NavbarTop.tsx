8f1829f6d901b1d202ea647b0e5e96fba20a2e27
Adam <trentowski.adam@gmail.com>
2023-11-06 10:07:58 +0100

    Refactor - split NotLoggedPage to smaller components

---
+++ Frontend/farmguider-app/src/components/NavbarTop/NavbarTop.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {AppBar, Slide, Toolbar} from '@mui/material';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import LanguageSwitcher from "@/components/LanguageSwitcher/LanguageSwitcher";
+import '@/components/NavbarTop/navbarTop.css';
+
+import logo from '@/assets/farm-guider.svg'
+
+type HideOnScrollProps = {
+    children: React.ReactElement;
+}
+
+function HideOnScroll(props: HideOnScrollProps) {
+    const {children} = props;
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 0,
+    });
+
+    return (
+        <Slide appear={false} direction="down" in={!trigger}>
+            {children}
+        </Slide>
+    );
+}
+
+const NavbarTop = () => {
+    return (
+        <>
+            <HideOnScroll>
+                <AppBar className="app-bar">
+                    <Toolbar className="toolbar">
+                        <img src={logo} alt="Farm guider logo" className='logo'/>
+                        <LanguageSwitcher/>
+                    </Toolbar>
+                </AppBar>
+            </HideOnScroll>
+            <Toolbar/>
+        </>
+    );
+};
+
+export default NavbarTop;
--- Frontend/farmguider-app/src/pages/NotLoggedPage/NotLoggedPage.tsx
+++ Frontend/farmguider-app/src/pages/NotLoggedPage/NotLoggedPage.tsx
@@ -1,39 +1,17 @@
-import React from 'react';
 import {Parallax} from 'react-parallax';
 import '@/pages/NotLoggedPage/notLoggedPage.css';
-import {AppBar, Box, Button, Toolbar, Typography} from '@mui/material';
-import useScrollTrigger from '@mui/material/useScrollTrigger';
-import Slide from '@mui/material/Slide';
+import {Box, Button, Typography} from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import GrassIcon from '@mui/icons-material/Grass';
 import TimelineIcon from '@mui/icons-material/Timeline';
 import SignalCellularAltIcon from '@mui/icons-material/SignalCellularAlt';
 import {Trans, useTranslation} from 'react-i18next';
-import LanguageSwitcher from "@/components/LanguageSwitcher/LanguageSwitcher.tsx";
+import NavbarTop from "@/components/NavbarTop/NavbarTop.tsx";
 
 import tractorImage from '@/assets/tractor-image.jpg';
 import cowsImage from '@/assets/cows-image.jpg';
 import cropsImage from '@/assets/crops-image.jpg';
 import wheatImage from '@/assets/wheat-image.jpg';
-import logo from '@/assets/farm-guider.svg'
-
-type HideOnScrollProps = {
-    children: React.ReactElement;
-}
-
-function HideOnScroll(props: HideOnScrollProps) {
-    const {children} = props;
-    const trigger = useScrollTrigger({
-        disableHysteresis: true,
-        threshold: 0,
-    });
-
-    return (
-        <Slide appear={false} direction="down" in={!trigger}>
-            {children}
-        </Slide>
-    );
-}
 
 const NotLoggedPage = () => {
     const dynamicBlur = {min: -1, max: 3};
@@ -44,22 +22,14 @@ const NotLoggedPage = () => {
 
     return (
         <Box>
-            <HideOnScroll>
-                <AppBar className="app-bar">
-                    <Toolbar className="toolbar">
-                        <img src={logo} alt="farm guider logo" className='logo'/>
-                        <LanguageSwitcher/>
-                    </Toolbar>
-                </AppBar>
-            </HideOnScroll>
-            <Toolbar/>
+            <NavbarTop/>
 
             <Box className="main-text-box">
                 <Typography className="header">
                     {t('mainTextBox.header')}
                 </Typography>
                 <Typography className="text">
-                    <Trans i18nKey='mainTextBox.text' ns='notLoggedPage' components={{ br: <br /> }} />
+                    <Trans i18nKey='mainTextBox.text' ns='notLoggedPage' components={{br: <br/>}}/>
                 </Typography>
                 <Button variant="outlined" color="primary" className="start-button">
                     {t('mainTextBox.button')}
@@ -80,7 +50,7 @@ const NotLoggedPage = () => {
             <Box className="section-text-box">
                 <GrassIcon color="secondary" className="icon"/>
                 <Typography className="text">
-                    <Trans i18nKey='crops.text' ns='notLoggedPage' components={{ br: <br /> }} />
+                    <Trans i18nKey='crops.text' ns='notLoggedPage' components={{br: <br/>}}/>
                 </Typography>
             </Box>
 
@@ -98,7 +68,7 @@ const NotLoggedPage = () => {
             <Box className="section-text-box">
                 <TimelineIcon color="secondary" className="icon"/>
                 <Typography className="text">
-                    <Trans i18nKey='animals.text' ns='notLoggedPage' components={{ br: <br /> }} />
+                    <Trans i18nKey='animals.text' ns='notLoggedPage' components={{br: <br/>}}/>
                 </Typography>
             </Box>
 
@@ -116,7 +86,7 @@ const NotLoggedPage = () => {
             <Box className="section-text-box">
                 <SignalCellularAltIcon color="secondary" className="icon"/>
                 <Typography className="text">
-                    <Trans i18nKey='efficiency.text' ns='notLoggedPage' components={{ br: <br /> }} />
+                    <Trans i18nKey='efficiency.text' ns='notLoggedPage' components={{br: <br/>}}/>
                 </Typography>
             </Box>
 
