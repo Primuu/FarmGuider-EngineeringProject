626b77cfa4c66686504f9a86f050b2874fd258ae
Adam <trentowski.adam@gmail.com>
2023-11-07 19:00:41 +0100

    Update OpenAPIConfig

--- Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/config/OpenAPIConfig.java
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/config/OpenAPIConfig.java
@@ -1,29 +1,15 @@
 package pl.edu.uwm.farmguider.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenAPIConfig {
-
-    final String securitySchemeName = "bearerAuth";
-
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement()
-                        .addList(securitySchemeName))
-                .components(new Components()
-                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
-                                .name(securitySchemeName)
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")))
                 .info(new Info()
                         .title("FarmGuider API")
                         .description("API to manage farm - animals and crops.")
