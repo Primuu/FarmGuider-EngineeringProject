63ce8c6ebec13d4f9bb7a84a278d62ec2f99e6a5
Adam <trentowski.adam@gmail.com>
2023-11-08 17:43:10 +0100

    Add snackbarStyles

--- Frontend/farmguider-app/src/App.tsx
+++ Frontend/farmguider-app/src/App.tsx
@@ -5,14 +5,21 @@ import '@/i18n.ts';
 import theme from "@/theme.ts";
 import AppRouter from "@/routes/AppRouter.tsx";
 import {SnackbarProvider} from "notistack";
+import {StyledMaterialDesignContent} from "@/styles/snackbarStyles.ts";
 
 function App() {
     return (
-        <SnackbarProvider maxSnack={3} autoHideDuration={5000}>
+        <SnackbarProvider
+            maxSnack={3}
+            autoHideDuration={5000}
+            Components={{
+                success: StyledMaterialDesignContent,
+            }}
+        >
             <AuthProvider>
                 <ThemeProvider theme={theme}>
                     <CssBaseline/>
-                    <AppRouter />
+                    <AppRouter/>
                 </ThemeProvider>
             </AuthProvider>
         </SnackbarProvider>
---
+++ Frontend/farmguider-app/src/styles/snackbarStyles.ts
@@ -0,0 +1,10 @@
+import {MaterialDesignContent } from 'notistack';
+import styled from '@emotion/styled';
+
+export const StyledMaterialDesignContent = styled(MaterialDesignContent)(() => ({
+    '&.notistack-MuiContent-root': {
+    },
+    '&.notistack-MuiContent-success': {
+        backgroundColor: '#2CB178',
+    },
+}));
