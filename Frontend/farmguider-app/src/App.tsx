8603cfea1c0a3db3bf3594c06aa59ff52a47d80b
Adam <trentowski.adam@gmail.com>
2023-11-08 17:27:55 +0100

    Add snackbar to app

--- Frontend/farmguider-app/src/App.tsx
+++ Frontend/farmguider-app/src/App.tsx
@@ -4,15 +4,18 @@ import CssBaseline from "@mui/material/CssBaseline";
 import '@/i18n.ts';
 import theme from "@/theme.ts";
 import AppRouter from "@/routes/AppRouter.tsx";
+import {SnackbarProvider} from "notistack";
 
 function App() {
     return (
-        <AuthProvider>
-            <ThemeProvider theme={theme}>
-                <CssBaseline/>
-                <AppRouter />
-            </ThemeProvider>
-        </AuthProvider>
+        <SnackbarProvider maxSnack={3} autoHideDuration={5000}>
+            <AuthProvider>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline/>
+                    <AppRouter />
+                </ThemeProvider>
+            </AuthProvider>
+        </SnackbarProvider>
     );
 }
 
