bb3c9e5815c57fc12c559540a3cba418ffbd7fa2
Adam <trentowski.adam@gmail.com>
2023-11-08 17:28:05 +0100

    Add snackbar variants

---
+++ Frontend/farmguider-app/src/utils/snackbarVariants.ts
@@ -0,0 +1,4 @@
+export const SnackbarSuccess = { variant: 'success' } as const;
+export const SnackbarError = { variant: 'error' } as const;
+export const SnackbarWarning = { variant: 'warning' } as const;
+export const SnackbarInfo = { variant: 'info' } as const;
