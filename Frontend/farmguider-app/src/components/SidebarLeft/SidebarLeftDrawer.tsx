9166387de95426422b75cf36327fc430db2fed26
Adam <trentowski.adam@gmail.com>
2023-11-13 11:51:50 +0100

    Add logo to Sidebar

--- Frontend/farmguider-app/src/components/SidebarLeft/SidebarLeftDrawer.tsx
+++ Frontend/farmguider-app/src/components/SidebarLeft/SidebarLeftDrawer.tsx
@@ -2,10 +2,13 @@ import List from "@mui/material/List";
 import {Box} from "@mui/material";
 import Typography from "@mui/material/Typography";
 
+import logo from "@/assets/farmguider-logo.svg";
+
 const SidebarLeftDrawer = () => (
     <div>
         <List>
             <Box className="logo-box">
+                <img src={logo} alt="Farm guider logo" className='logo'/>
                 <Typography className="logo-text">
                     FarmGuider
                 </Typography>
