e1e8493796316c8510f20180c851b6d10fed3716
Adam <trentowski.adam@gmail.com>
2023-11-15 16:59:55 +0100

    Add profile page and locales to router config

--- Frontend/farmguider-app/src/constants/ROUTER_URLS.ts
+++ Frontend/farmguider-app/src/constants/ROUTER_URLS.ts
@@ -1,2 +1,3 @@
 export const NOT_LOGGED_PAGE_URL: string = "/";
 export const HOME_PAGE_URL: string = "/home";
+export const PROFILE_PAGE_URL: string = "/profile";
--- Frontend/farmguider-app/src/routes/routerConfig.ts
+++ Frontend/farmguider-app/src/routes/routerConfig.ts
@@ -1,12 +1,15 @@
 import UserRoles from "@/contexts/AuthContext/UserRoles.ts";
 import {RouteConfig} from "@/routes/AppRouter.tsx";
-import {HOME_PAGE_URL, NOT_LOGGED_PAGE_URL} from "@/constants/ROUTER_URLS.ts";
+import {HOME_PAGE_URL, NOT_LOGGED_PAGE_URL, PROFILE_PAGE_URL} from "@/constants/ROUTER_URLS.ts";
 import NotLoggedPage from "@/pages/NotLoggedPage/NotLoggedPage.tsx";
 import HomePage from "@/pages/HomePage/HomePage.tsx";
+import ProfilePage from "@/pages/ProfilePage/ProfilePage.tsx";
+import i18n from "@/i18n.ts";
 
 const routesConfig: RouteConfig[]= [
-    { path: NOT_LOGGED_PAGE_URL, page: NotLoggedPage, accessibleRoles: UserRoles.NON_LOGGED, title: "FarmGuider" },
-    { path: HOME_PAGE_URL, page: HomePage, accessibleRoles: UserRoles.ROLE_USER, title: "FarmGuider" },
+    { path: NOT_LOGGED_PAGE_URL, page: NotLoggedPage, accessibleRoles: UserRoles.NON_LOGGED, title: i18n.t('titles:default') },
+    { path: HOME_PAGE_URL, page: HomePage, accessibleRoles: UserRoles.ROLE_USER, title: i18n.t('titles:default')},
+    { path: PROFILE_PAGE_URL, page: ProfilePage, accessibleRoles: UserRoles.ROLE_USER, title: i18n.t('titles:profile') },
 ];
 
 export default routesConfig;
