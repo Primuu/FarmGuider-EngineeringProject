a133561e8ab79503715c5b3202678d08c9520b56
Adam <trentowski.adam@gmail.com>
2023-11-15 16:59:09 +0100

    Add profile page placeholder

---
+++ Frontend/farmguider-app/src/pages/ProfilePage/ProfilePage.tsx
@@ -0,0 +1,11 @@
+import '@/pages/ProfilePage/profilePage.css';
+
+const ProfilePage = () => {
+    return (
+        <div className="profile-container">
+            Profile Placeholder
+        </div>
+    )
+}
+
+export default ProfilePage;
