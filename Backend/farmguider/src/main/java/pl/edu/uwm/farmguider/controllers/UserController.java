faf7280491694fcca76c80ee5198d818fd36d914
Adam <trentowski.adam@gmail.com>
2023-11-22 12:07:47 +0100

    Add getUserById rest and docs to it

--- Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/controllers/UserController.java
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/controllers/UserController.java
@@ -1,10 +1,21 @@
 package pl.edu.uwm.farmguider.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.edu.uwm.farmguider.exceptions.ErrorResponse;
 import pl.edu.uwm.farmguider.facades.UserFacade;
+import pl.edu.uwm.farmguider.models.user.dtos.UserResponseDTO;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,5 +24,29 @@ import pl.edu.uwm.farmguider.facades.UserFacade;
 public class UserController {
 
     private final UserFacade userFacade;
+
+    @Operation(summary = "Get user data by id", description = "Retrieves a user's data by their id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "User data retrieved successfully",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = UserResponseDTO.class)
+                    )),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Not Found - User not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    ))
+    })
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponseDTO> getUserById(@PathVariable Long userId) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(userFacade.getUserById(userId));
+    }
 
 }
