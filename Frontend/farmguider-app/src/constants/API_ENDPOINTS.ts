273829b273a79074524c08a026592b79f50ff4c3
Adam <trentowski.adam@gmail.com>
2023-11-22 13:29:12 +0100

    Add GET_USER_DATA_URL to API_ENDPOINTS

--- Frontend/farmguider-app/src/constants/API_ENDPOINTS.ts
+++ Frontend/farmguider-app/src/constants/API_ENDPOINTS.ts
@@ -1,7 +1,10 @@
 export const BASE_URL: string = "http://localhost:8080";
 
-//Authentication
+// Authentication
 export const REGISTER_URL: string = "/register";
 export const AUTHENTICATE_URL: string = "/authenticate";
 export const REVOKE_URL: string = "/revoke";
 export const GET_AUTH_DATA_URL: string = "/auth-data";
+
+// User
+export const GET_USER_DATA_URL: string = "/user/{userId}";
