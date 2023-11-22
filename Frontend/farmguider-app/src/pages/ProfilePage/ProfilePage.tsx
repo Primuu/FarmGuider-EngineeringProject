e89f3795ebffbda4942967f24c02092d9fa1b169
Adam <trentowski.adam@gmail.com>
2023-11-22 16:23:54 +0100

    Add ProfilePage prototype

--- Frontend/farmguider-app/src/pages/ProfilePage/ProfilePage.tsx
+++ Frontend/farmguider-app/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,9 +1,47 @@
 import '@/pages/ProfilePage/profilePage.css';
+import {useAuth} from "@/contexts/AuthContext/AuthContext.tsx";
+import {useEffect, useState} from "react";
+import {fetchUserData} from "@/services/userService.ts";
+import LoadingScreen from "@/components/LoadingScreen/LoadingScreen.tsx";
+import UserResponseDTO from "@/entities/UserResponseDTO.ts";
+import {useNavigate} from "react-router-dom";
+import {NOT_FOUND_PAGE_URL} from "@/constants/ROUTER_URLS.ts";
+import Typography from "@mui/material/Typography";
 
 const ProfilePage = () => {
+    const {userId} = useAuth();
+    const [userResponseDTO, setUserResponseDTO] = useState<UserResponseDTO | null>(null);
+    const [loading, setLoading] = useState(true);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (userId) {
+            fetchUserData(userId)
+                .then(data => {
+                    setUserResponseDTO(data);
+                    setLoading(false);
+                })
+                .catch(() => {
+                    setLoading(false);
+                    navigate(NOT_FOUND_PAGE_URL, {replace: true});
+                })
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [userId]);
+
+    if (loading) return <LoadingScreen/>;
+    if (!userResponseDTO) return null;
+
     return (
-        <div className="profile-container">
-            Profile Placeholder
+        <div>
+            <Typography className="profile-header">
+                Profile
+            </Typography>
+            <div className="profile-container">
+                {/*<div>User ID: {userResponseDTO.userId}</div>*/}
+                {/*<div>Name: {userResponseDTO.firstName}</div>*/}
+                {/*<div>Email: {userResponseDTO.email}</div>*/}
+            </div>
         </div>
     )
 }
