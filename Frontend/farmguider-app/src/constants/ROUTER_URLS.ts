97507c9764505596dd8885f0dfb779fd5659677d
Adam <trentowski.adam@gmail.com>
2023-11-22 13:30:01 +0100

    Add NOT_FOUND_PAGE_URL to ROUTER_URLS

--- Frontend/farmguider-app/src/constants/ROUTER_URLS.ts
+++ Frontend/farmguider-app/src/constants/ROUTER_URLS.ts
@@ -1,3 +1,4 @@
 export const NOT_LOGGED_PAGE_URL: string = "/";
+export const NOT_FOUND_PAGE_URL: string = "/not-found";
 export const HOME_PAGE_URL: string = "/home";
 export const PROFILE_PAGE_URL: string = "/profile";
