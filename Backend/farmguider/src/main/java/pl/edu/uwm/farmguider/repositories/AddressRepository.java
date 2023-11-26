5c12536f262a6c2146f02dcd17b67967e032b88d
Adam <trentowski.adam@gmail.com>
2023-11-26 18:43:57 +0100

    Add findAddressByUserId method in AddressRepository

--- Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/repositories/AddressRepository.java
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/repositories/AddressRepository.java
@@ -1,7 +1,21 @@
 package pl.edu.uwm.farmguider.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.edu.uwm.farmguider.models.address.Address;
 
+import java.util.Optional;
+
 public interface AddressRepository extends JpaRepository<Address, Long> {
+
+    @Query("""
+            SELECT a
+            FROM Address a
+            INNER JOIN User u
+            ON a.id = u.address.id
+            WHERE u.id = :userId
+            """)
+    Optional<Address> findAddressByUserId(@Param("userId") Long userId);
+
 }
