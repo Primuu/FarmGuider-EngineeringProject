aa9e67eb75556d9c334263b080698f5e88c7890a
Adam <trentowski.adam@gmail.com>
2023-11-26 18:44:24 +0100

    Add getAddressByUserId and updateAddressByUserId methods in AddressService

--- Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/services/AddressService.java
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/services/AddressService.java
@@ -2,6 +2,7 @@ package pl.edu.uwm.farmguider.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.edu.uwm.farmguider.exceptions.global.EntityNotFoundException;
 import pl.edu.uwm.farmguider.models.address.Address;
 import pl.edu.uwm.farmguider.repositories.AddressRepository;
 
@@ -11,8 +12,22 @@ public class AddressService {
 
     private final AddressRepository addressRepository;
 
+    public Address getAddressByUserId(Long userId) {
+        return addressRepository.findAddressByUserId(userId)
+                .orElseThrow(() -> new EntityNotFoundException("Address", "Address for user with id: " + userId + " not found."));
+    }
+
     public Address createEmptyAddress() {
         return addressRepository.saveAndFlush(new Address());
+    }
+
+    public void updateAddressByUserId(Long userId, String locality, String street, String zipCode, String propertyNumber) {
+        Address address = getAddressByUserId(userId);
+        address.setLocality(locality);
+        address.setStreet(street);
+        address.setZipCode(zipCode);
+        address.setPropertyNumber(propertyNumber);
+        addressRepository.saveAndFlush(address);
     }
 
 }
