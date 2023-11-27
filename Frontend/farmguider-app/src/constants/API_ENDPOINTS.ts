b2c056d50e3cf0249882ca71afb33583cbb62c04
Adam <trentowski.adam@gmail.com>
2023-11-27 20:01:42 +0100

    Add api urls to constants, move change password method to userService, add fetching method to delete account

--- Frontend/farmguider-app/src/constants/API_ENDPOINTS.ts
+++ Frontend/farmguider-app/src/constants/API_ENDPOINTS.ts
@@ -5,8 +5,9 @@ export const REGISTER_URL: string = "/register";
 export const AUTHENTICATE_URL: string = "/authenticate";
 export const REVOKE_URL: string = "/revoke";
 export const GET_AUTH_DATA_URL: string = "/auth-data";
-export const CHANGE_PASSWORD_URL: string = "/change-passwd";
 
 // User
 export const GET_USER_DATA_URL: string = "/user/{userId}";
 export const UPDATE_USER_URL: string = "/user/update/{userId}";
+export const CHANGE_PASSWORD_URL: string = "/user/change-password";
+export const DELETE_ACCOUNT_URL: string = "/user/delete-account";
--- Frontend/farmguider-app/src/services/userService.ts
+++ Frontend/farmguider-app/src/services/userService.ts
@@ -1,8 +1,16 @@
 import sendHttpRequest from "@/services/HttpRequestService/HttpRequestService.ts";
-import {GET_USER_DATA_URL, UPDATE_USER_URL} from "@/constants/API_ENDPOINTS.ts";
+import {
+    CHANGE_PASSWORD_URL,
+    DELETE_ACCOUNT_URL,
+    GET_USER_DATA_URL,
+    UPDATE_USER_URL
+} from "@/constants/API_ENDPOINTS.ts";
 import RequestTypes from "@/services/HttpRequestService/RequestTypes.ts";
 import UserResponseDTO from "@/entities/UserResponseDTO.ts";
 import UserUpdateDTO from "@/entities/UserUpdateDTO.ts";
+import UserChangePasswordDTO from "@/entities/UserChangePasswordDTO.ts";
+import ResponseMessage from "@/entities/ResponseMessage.ts";
+import UserPasswordDTO from "@/entities/UserPasswordDTO.ts";
 
 export const fetchUserData = async (userId: number): Promise<UserResponseDTO> => {
     return await sendHttpRequest<UserResponseDTO>({
@@ -18,5 +26,21 @@ export const updateUser = async (userId: number, userUpdateDTO: UserUpdateDTO): 
         typeOfRequest: RequestTypes.PUT,
         pathParams: { userId },
         body: userUpdateDTO
+    });
+}
+
+export const changePassword = async (userChangePasswordDTO: UserChangePasswordDTO): Promise<ResponseMessage>  => {
+    return await sendHttpRequest<ResponseMessage>({
+        endpointUrl: CHANGE_PASSWORD_URL,
+        typeOfRequest: RequestTypes.PATCH,
+        body: userChangePasswordDTO
+    });
+}
+
+export const deleteAccount = async (userPasswordDTO: UserPasswordDTO): Promise<ResponseMessage>  => {
+    return await sendHttpRequest<ResponseMessage>({
+        endpointUrl: DELETE_ACCOUNT_URL,
+        typeOfRequest: RequestTypes.DELETE,
+        body: userPasswordDTO
     });
 }
