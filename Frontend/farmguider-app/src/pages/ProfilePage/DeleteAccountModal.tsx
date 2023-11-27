f4b55beadf4267c10266a57af1e58cbed5fdec46
Adam <trentowski.adam@gmail.com>
2023-11-27 20:07:39 +0100

    Update DeleteAccountModal

--- Frontend/farmguider-app/src/pages/ProfilePage/DeleteAccountModal.tsx
+++ Frontend/farmguider-app/src/pages/ProfilePage/DeleteAccountModal.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import {useTranslation} from "react-i18next";
-import {Box, Fade, Modal, Slide} from "@mui/material";
+import {Box, Button, Fade, IconButton, InputAdornment, Modal, Slide, TextField, Typography} from "@mui/material";
+import WarningIcon from '@mui/icons-material/Warning';
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import {SnackbarSuccess} from "@/utils/snackbarVariants.ts";
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import {deleteAccount} from "@/services/userService.ts";
+import UserPasswordDTO from "@/entities/UserPasswordDTO.ts";
+import {useAuth} from "@/contexts/AuthContext/AuthContext.tsx";
+import {useSnackbar} from "notistack";
 
-import '@/pages/ProfilePage/modals.css';
+import '@/pages/ProfilePage/deleteAccount.css';
 
 type DeleteAccountModalProps = {
     open: boolean;
@@ -10,25 +20,116 @@ type DeleteAccountModalProps = {
 }
 
 const DeleteAccountModal: React.FC<DeleteAccountModalProps> = ({open, onClose}) => {
+    const [password, setPassword] = useState<string>('');
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
     const {t} = useTranslation('profilePage');
+    const {removeSessionCookie} = useAuth();
+    const {enqueueSnackbar} = useSnackbar();
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    };
+
+    const cancel = () => {
+        setPassword("");
+        setError(null);
+        onClose();
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const userPasswordDTO: UserPasswordDTO = {
+            password: password
+        };
+
+        deleteAccount(userPasswordDTO)
+            .then(() => {
+                removeSessionCookie();
+                enqueueSnackbar(t('deleteAccount.snackbar'), SnackbarSuccess);
+            })
+            .catch(() => {
+                 setError(t('deleteAccount.error'));
+            })
+    };
 
     return (
         <Modal
             open={open}
-            onClose={onClose}
+            onClose={cancel}
             closeAfterTransition
         >
             <Fade in={open}>
-                <Box className="profile-modal-box">
+                <Box className="delete-acc-modal-box">
                     <Slide
                         direction="right"
                         in={open}
                         mountOnEnter
                         unmountOnExit
                     >
-                        <div>
-                            DELETE ACCOUNT
-                        </div>
+                        <Box component="form" onSubmit={handleSubmit} className="delete-acc-form">
+                            <Typography className="delete-acc-header">
+                                <WarningIcon className="delete-acc-icon"/>
+                                {t('deleteAccount.header')}
+                            </Typography>
+
+                            <Typography className="delete-acc-instruction">
+                                {t('deleteAccount.instruction')}
+                            </Typography>
+
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                autoComplete="new-password"
+                                label={t('deleteAccount.password')}
+                                type={showPassword ? "text" : "password"}
+                                value={password}
+                                onChange={handlePasswordChange}
+                                error={!!error}
+                                helperText={error}
+                                InputProps={{
+                                    endAdornment: (
+                                        <InputAdornment position="end">
+                                            <IconButton
+                                                onClick={togglePasswordVisibility}
+                                            >
+                                                {showPassword ? <Visibility/> : <VisibilityOff/>}
+                                            </IconButton>
+                                        </InputAdornment>
+                                    ),
+                                }}
+                            />
+                            <Typography className="delete-acc-text">
+                                {t('deleteAccount.warning')}
+                            </Typography>
+
+                            <Box className="delete-acc-button-group">
+                                <Button
+                                    type="submit"
+                                    variant="contained"
+                                    className="confirm-delete-acc-button"
+                                    color="secondary"
+                                >
+                                    <DeleteOutlineIcon className="delete-acc-button-icon"/>
+                                    {t('deleteAccount.deleteAccountButton')}
+                                </Button>
+                                <Button
+                                    variant="outlined"
+                                    onClick={cancel}
+                                    className="cancel-delete-acc-button"
+                                    color="secondary"
+                                >
+                                    <CloseOutlinedIcon className="delete-acc-button-icon"/>
+                                    {t('deleteAccount.cancelButton')}
+                                </Button>
+                            </Box>
+                        </Box>
                     </Slide>
                 </Box>
             </Fade>
