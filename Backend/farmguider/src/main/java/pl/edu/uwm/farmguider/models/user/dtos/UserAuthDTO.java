2b904e49e05495c21c71b4261c7b38ccfc0fe071
Adam <trentowski.adam@gmail.com>
2023-11-29 19:52:31 +0100

    Add farmId to UserAuthDTO

--- Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/models/user/dtos/UserAuthDTO.java
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/models/user/dtos/UserAuthDTO.java
@@ -3,6 +3,7 @@ package pl.edu.uwm.farmguider.models.user.dtos;
 import lombok.Builder;
 
 public record UserAuthDTO(Long userId,
+                          Long farmId,
                           String userRole) {
 
     @Builder
