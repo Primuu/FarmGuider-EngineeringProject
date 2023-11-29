36502ee9752490dcadc2f5a3d86dce1bb7dc9167
Adam <trentowski.adam@gmail.com>
2023-11-29 17:54:14 +0100

    Add findFarmByOwnerEmail method in FarmRepository

--- Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/repositories/FarmRepository.java
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/repositories/FarmRepository.java
@@ -1,8 +1,19 @@
 package pl.edu.uwm.farmguider.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.edu.uwm.farmguider.models.farm.Farm;
 
+import java.util.Optional;
+
 public interface FarmRepository extends JpaRepository<Farm, Long> {
+
+    @Query("""
+            SELECT f
+            FROM Farm f
+            WHERE f.owner.email = :email
+            """)
+    Optional<Farm> findFarmByOwnerEmail(@Param("email") String email);
 
 }
