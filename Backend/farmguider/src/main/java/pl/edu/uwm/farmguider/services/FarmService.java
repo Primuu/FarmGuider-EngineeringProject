64ba4d96d2ba439beb6071127617931cc0e67734
Adam <trentowski.adam@gmail.com>
2023-11-29 17:54:58 +0100

    Add getFarmById, getFarmByOwnerEmail and deleteFarm methods in FarmService

--- Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/services/FarmService.java
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/services/FarmService.java
@@ -2,6 +2,7 @@ package pl.edu.uwm.farmguider.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.edu.uwm.farmguider.exceptions.global.EntityNotFoundException;
 import pl.edu.uwm.farmguider.models.farm.Farm;
 import pl.edu.uwm.farmguider.models.user.User;
 import pl.edu.uwm.farmguider.repositories.FarmRepository;
@@ -11,6 +12,19 @@ import pl.edu.uwm.farmguider.repositories.FarmRepository;
 public class FarmService {
 
     private final FarmRepository farmRepository;
+
+    public Farm getFarmById(Long farmId) {
+        return farmRepository.findById(farmId)
+                .orElseThrow(() -> new EntityNotFoundException("Farm", "Farm with id: " + farmId + " not found."));
+    }
+
+    public Farm getFarmByOwnerEmail(String email) {
+        return farmRepository.findFarmByOwnerEmail(email)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        "Farm",
+                        "Farm for user with email: " + email + " not found."
+                ));
+    }
 
     public Farm createFarm(User owner) {
         Farm farm = new Farm(
@@ -22,6 +36,11 @@ public class FarmService {
 
     private String createFarmName(Long ownerId) {
         return "Farm #" + ownerId.toString();
+    }
+
+    public void deleteFarmByOwnerEmail(String email) {
+        Farm farm = getFarmByOwnerEmail(email);
+        farmRepository.delete(farm);
     }
 
 }
