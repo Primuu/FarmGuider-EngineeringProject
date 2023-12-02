e5a3d096bb3754e156f162d8f272d77e095cf211
Adam <trentowski.adam@gmail.com>
2023-12-02 09:16:50 +0100

    Add BreedingPage to routerConfig

--- Frontend/farmguider-app/src/constants/ROUTER_URLS.ts
+++ Frontend/farmguider-app/src/constants/ROUTER_URLS.ts
@@ -2,3 +2,4 @@ export const NOT_LOGGED_PAGE_URL: string = "/";
 export const NOT_FOUND_PAGE_URL: string = "/not-found";
 export const HOME_PAGE_URL: string = "/home";
 export const PROFILE_PAGE_URL: string = "/profile";
+export const BREEDING_PAGE_URL: string = "/breeding";
--- Frontend/farmguider-app/src/routes/routerConfig.ts
+++ Frontend/farmguider-app/src/routes/routerConfig.ts
@@ -1,14 +1,16 @@
 import UserRoles from "@/contexts/AuthContext/UserRoles.ts";
 import {RouteConfig} from "@/routes/AppRouter.tsx";
-import {HOME_PAGE_URL, NOT_LOGGED_PAGE_URL, PROFILE_PAGE_URL} from "@/constants/ROUTER_URLS.ts";
+import {BREEDING_PAGE_URL, HOME_PAGE_URL, NOT_LOGGED_PAGE_URL, PROFILE_PAGE_URL} from "@/constants/ROUTER_URLS.ts";
 import NotLoggedPage from "@/pages/NotLoggedPage/NotLoggedPage.tsx";
 import HomePage from "@/pages/HomePage/HomePage.tsx";
 import ProfilePage from "@/pages/ProfilePage/ProfilePage.tsx";
+import BreedingPage from "@/pages/BreedingPage/BreedingPage.tsx";
 
 const routesConfig: RouteConfig[] = [
     {path: NOT_LOGGED_PAGE_URL, page: NotLoggedPage, accessibleRoles: UserRoles.NON_LOGGED},
     {path: HOME_PAGE_URL, page: HomePage, accessibleRoles: UserRoles.ROLE_USER},
     {path: PROFILE_PAGE_URL, page: ProfilePage, accessibleRoles: UserRoles.ROLE_USER},
+    {path: BREEDING_PAGE_URL, page: BreedingPage, accessibleRoles: UserRoles.ROLE_USER},
 ];
 
 export default routesConfig;
