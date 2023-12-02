e8ada95b0ea8a941bf2c218e7d0e5eb2a98fb33a
Adam <trentowski.adam@gmail.com>
2023-12-02 11:29:52 +0100

    Add NoBreedingContent

--- Frontend/farmguider-app/src/pages/BreedingPage/NoBreedingContent.tsx
+++ Frontend/farmguider-app/src/pages/BreedingPage/NoBreedingContent.tsx
@@ -1,7 +1,29 @@
+import {useTranslation} from "react-i18next";
+import {Button} from "@mui/material";
+import Typography from "@mui/material/Typography";
+import AddIcon from '@mui/icons-material/Add';
+
+import '@/pages/BreedingPage/noBreedingContent.css';
+import cowImage from "@/assets/cow-image.png";
+
 export const NoBreedingContent = () => {
+    const {t} = useTranslation('breedingPage');
+
     return (
-        <div>
-            No breeding
+        <div className="no-breeding-container">
+            <img src={cowImage} alt={t('noBreedingContent.imageAlt')} className='cow'/>
+            <Typography className="no-breeding-header">
+                {t('noBreedingContent.information')}
+            </Typography>
+            <Button
+                className="add-herd-button-no-content"
+                variant="contained"
+                color="primary"
+                // onClick={}
+            >
+                <AddIcon className="add-herd-button-icon-no-content"/>
+                {t('noBreedingContent.addButton')}
+            </Button>
         </div>
     )
 }
