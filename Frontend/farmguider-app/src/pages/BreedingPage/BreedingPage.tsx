6683773ca9fbd1c7f0307c7ed62c8f10553382e1
Adam <trentowski.adam@gmail.com>
2023-12-02 09:16:26 +0100

    Add BreedingPage placeholder

---
+++ Frontend/farmguider-app/src/pages/BreedingPage/BreedingPage.tsx
@@ -0,0 +1,11 @@
+import '@/pages/BreedingPage/breedingPage.css';
+
+const BreedingPage = () => {
+    return (
+        <div>
+            Breeding Placeholder
+        </div>
+    )
+}
+
+export default BreedingPage;
