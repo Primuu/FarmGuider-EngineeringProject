07a1d99f8b6c66f2cf0ff7a4209785d93e2481b2
Adam <trentowski.adam@gmail.com>
2023-12-02 19:22:00 +0100

    Add findUserIdByBreedingId to BreedingRepository

--- Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/repositories/BreedingRepository.java
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/repositories/BreedingRepository.java
@@ -1,6 +1,8 @@
 package pl.edu.uwm.farmguider.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.edu.uwm.farmguider.models.breeding.Breeding;
 
 import java.util.List;
@@ -8,5 +10,13 @@ import java.util.List;
 public interface BreedingRepository extends JpaRepository<Breeding, Long> {
 
     List<Breeding> findAllByFarmId(Long farmId);
+
+    @Query("""
+           SELECT b.farm.owner.id
+           FROM Breeding b
+           JOIN b.farm.owner
+           WHERE b.id = :breedingId
+           """)
+    Long findUserIdByBreedingId(@Param("breedingId") Long breedingId);
 
 }
