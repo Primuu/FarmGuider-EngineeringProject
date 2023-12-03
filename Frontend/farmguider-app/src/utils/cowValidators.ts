1ce18ebcb95f77ce5f554d155c7c8243da412a67
Adam <trentowski.adam@gmail.com>
2023-12-03 20:47:35 +0100

    Update cowValidators

--- Frontend/farmguider-app/src/utils/cowValidators.ts
+++ Frontend/farmguider-app/src/utils/cowValidators.ts
@@ -5,19 +5,22 @@ export const EAR_TAG_REGEX = /^[A-Z]{2}\d{12}$/;
 export type CowValues = {
     earTagNumber: string | null;
     cowName: string | null;
+    dateOfBirth: Date | null;
 }
 
 export type CowErrors = {
     earTagNumber?: string;
     cowName?: string;
+    dateOfBirth?: string;
 }
 
 export const validateAddCow = (values: CowValues, t: TFunction): CowErrors => {
-    const {earTagNumber, cowName} = values;
+    const {earTagNumber, cowName, dateOfBirth} = values;
     const tempErrors: CowErrors = {};
 
     tempErrors.earTagNumber = validateEarTagNumber(earTagNumber, t);
     tempErrors.cowName = validateCowName(cowName, t);
+    tempErrors.dateOfBirth = validateDateOfBirth(dateOfBirth, t);
 
     return tempErrors;
 }
@@ -31,5 +34,11 @@ export const validateEarTagNumber = (earTagNumber: string | null, t: TFunction):
     if (!earTagNumber) return t('addCowModal.validation.earTagNumberRequired');
     if (earTagNumber.length !== 14) return t('addCowModal.validation.earTagNumberLength');
     if (!EAR_TAG_REGEX.test(earTagNumber)) return t('addCowModal.validation.earTagNumberPattern');
+    return '';
+};
+
+export const validateDateOfBirth = (dateOfBirth: Date | null, t: TFunction): string => {
+    if (!dateOfBirth) return t('addCowModal.validation.dateOfBirthRequired');
+    if (dateOfBirth > new Date()) return t('addCowModal.validation.dateOfBirthPastOrPresent');
     return '';
 };
