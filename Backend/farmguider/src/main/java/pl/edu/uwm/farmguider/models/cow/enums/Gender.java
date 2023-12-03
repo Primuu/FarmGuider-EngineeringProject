e0ff18de3008f9ef0c1100b8c5e91e90f7aba64c
Adam <trentowski.adam@gmail.com>
2023-12-03 14:14:16 +0100

    Update Gender enum

--- Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/models/cow/enums/Gender.java
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/models/cow/enums/Gender.java
@@ -1,7 +1,14 @@
 package pl.edu.uwm.farmguider.models.cow.enums;
 
+import java.util.Arrays;
+
 public enum Gender {
     MALE,
-    FEMALE
+    FEMALE;
+
+    public static boolean isValid(String value) {
+        return Arrays.stream(Gender.values())
+                .anyMatch(gender -> gender.name().equals(value));
+    }
 
 }
