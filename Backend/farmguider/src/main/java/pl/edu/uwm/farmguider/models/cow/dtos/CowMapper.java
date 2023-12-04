2f969c3073126e3635b80fe90eff2217e690ae50
Adam <trentowski.adam@gmail.com>
2023-12-04 15:01:11 +0100

    Update CowMapper

--- Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/models/cow/dtos/CowMapper.java
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/models/cow/dtos/CowMapper.java
@@ -2,6 +2,8 @@ package pl.edu.uwm.farmguider.models.cow.dtos;
 
 import pl.edu.uwm.farmguider.models.cow.Cow;
 
+import java.math.BigDecimal;
+
 public class CowMapper {
 
     public static CowResponseDTO mapToCowResponseDTO(Cow cow) {
@@ -11,6 +13,20 @@ public class CowMapper {
                 .earTagNumber(cow.getEarTagNumber())
                 .dateOfBirth(cow.getDateOfBirth())
                 .gender(cow.getGender().name())
+                .build();
+    }
+
+    public static CowPageResponseDTO mapToCowPageResponseDTO(Cow cow,
+                                                             BigDecimal currentWeight,
+                                                             BigDecimal latestMilkingQuantity) {
+        return CowPageResponseDTO.builder()
+                .cowId(cow.getId())
+                .cowName(cow.getCowName())
+                .earTagNumber(cow.getEarTagNumber())
+                .dateOfBirth(cow.getDateOfBirth())
+                .gender(cow.getGender().name())
+                .currentWeight(currentWeight)
+                .latestMilkingQuantity(latestMilkingQuantity)
                 .build();
     }
 
