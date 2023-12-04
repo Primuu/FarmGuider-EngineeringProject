78795fc7bc9469d5e400d560d6093af247258808
Adam <trentowski.adam@gmail.com>
2023-12-04 15:29:07 +0100

    Update CowResponseDTO

--- Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/models/cow/dtos/CowMapper.java
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/models/cow/dtos/CowMapper.java
@@ -2,8 +2,6 @@ package pl.edu.uwm.farmguider.models.cow.dtos;
 
 import pl.edu.uwm.farmguider.models.cow.Cow;
 
-import java.math.BigDecimal;
-
 public class CowMapper {
 
     public static CowResponseDTO mapToCowResponseDTO(Cow cow) {
@@ -13,20 +11,8 @@ public class CowMapper {
                 .earTagNumber(cow.getEarTagNumber())
                 .dateOfBirth(cow.getDateOfBirth())
                 .gender(cow.getGender().name())
-                .build();
-    }
-
-    public static CowPageResponseDTO mapToCowPageResponseDTO(Cow cow,
-                                                             BigDecimal currentWeight,
-                                                             BigDecimal latestMilkingQuantity) {
-        return CowPageResponseDTO.builder()
-                .cowId(cow.getId())
-                .cowName(cow.getCowName())
-                .earTagNumber(cow.getEarTagNumber())
-                .dateOfBirth(cow.getDateOfBirth())
-                .gender(cow.getGender().name())
-                .currentWeight(currentWeight)
-                .latestMilkingQuantity(latestMilkingQuantity)
+                .currentWeight(cow.getCurrentWeight())
+                .latestMilkingQuantity(cow.getLatestMilkingQuantity())
                 .build();
     }
 
--- Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/models/cow/dtos/CowResponseDTO.java
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/models/cow/dtos/CowResponseDTO.java
@@ -2,13 +2,16 @@ package pl.edu.uwm.farmguider.models.cow.dtos;
 
 import lombok.Builder;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record CowResponseDTO(Long cowId,
                              String cowName,
                              String earTagNumber,
                              LocalDate dateOfBirth,
-                             String gender) {
+                             String gender,
+                             BigDecimal currentWeight,
+                             BigDecimal latestMilkingQuantity) {
 
     @Builder
     public CowResponseDTO {}
