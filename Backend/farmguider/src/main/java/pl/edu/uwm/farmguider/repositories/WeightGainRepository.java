cb280660961efc99546e1f39f1af05ad65faae4c
Adam <trentowski.adam@gmail.com>
2023-12-04 15:00:33 +0100

    Add getCurrentWeightByCowId method

--- Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/repositories/WeightGainRepository.java
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/repositories/WeightGainRepository.java
@@ -1,8 +1,21 @@
 package pl.edu.uwm.farmguider.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.edu.uwm.farmguider.models.weightGain.WeightGain;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface WeightGainRepository extends JpaRepository<WeightGain, Long> {
+
+    @Query("""
+            SELECT wg.weight
+            FROM WeightGain wg
+            WHERE wg.cow.id = :cowId
+            ORDER BY wg.measurementDate DESC
+            """)
+    List<BigDecimal> findLatestWeightsByCowId(@Param("cowId") Long cowId);
 
 }
--- Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/services/WeightGainService.java
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/services/WeightGainService.java
@@ -8,6 +8,7 @@ import pl.edu.uwm.farmguider.repositories.WeightGainRepository;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +19,11 @@ public class WeightGainService {
     public WeightGain createWeightGain(Cow cow, LocalDate measurementDate, BigDecimal weight) {
         WeightGain weightGain = new WeightGain(cow, measurementDate, weight);
         return weightGainRepository.saveAndFlush(weightGain);
+    }
+
+    public BigDecimal getCurrentWeightByCowId(Long cowId) {
+        List<BigDecimal> weights = weightGainRepository.findLatestWeightsByCowId(cowId);
+        return weights.isEmpty() ? null : weights.get(0);
     }
 
 }
