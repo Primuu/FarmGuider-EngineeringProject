caf510f385f469281244e411d36e01c17879320c
Adam <trentowski.adam@gmail.com>
2023-12-04 18:41:20 +0100

    Add getCowsByBreedingId rest docs

--- Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/controllers/CowController.java
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/controllers/CowController.java
@@ -1,6 +1,7 @@
 package pl.edu.uwm.farmguider.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -28,6 +29,37 @@ public class CowController {
 
     private final CowFacade cowFacade;
 
+    @Operation(summary = "Get cow's data by breeding id",
+            description = """
+                    Complex rest for searching cows in breeding with the option of filtering and sorting.
+                    The following parameters are used for pagination:
+                    - page: specifies the page (default 0, minimum value = 0)
+                    - size: determines the number of entries on the page (default 25, minimum  = 1, maximum = 100)
+                                        
+                    Can filter by fields:
+                    - earTagNumber: searches for matching earring numbers (no need to enter the entire number)
+                    - gender: enter "FEMALE", "MALE" or do not enter this field at all
+                    - cowName: searches for matching cow names, case-insensitive (no need to enter the entire name)
+                    - minDateOfBirth and maxDateOfBirth: provide a range of animals birth dates (do not have to provide both dates)
+                    - minWeight and maxWeight: provide a range of animal weights (do not need to provide both weights)
+                    - minMilkingQuantity and maxMilkingQuantity: provide a range of milk quantities from the last milking (do not need to provide both values)
+                                        
+                    Sorting is done using fields:
+                    - sortDesc: sorting direction (ascending by default)
+                    - sortBy: field by which cows will be sorted (earTagNumber by default).
+                    Can provide the following values: cowName, earTagNumber, dateOfBirth, currentWeight, latestMilkingQuantity, gender
+                    """)
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Breeding page retrieved successfully",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(
+                                    schema = @Schema(implementation = CowResponseDTO.class)
+                            )
+                    ))
+    })
     @GetMapping("/get-cows/{breedingId}")
     public ResponseEntity<Page<CowResponseDTO>> getCowsByBreedingId(@PathVariable Long breedingId,
                                                                     @ModelAttribute @Valid CowSearchParams cowSearchParams) {
