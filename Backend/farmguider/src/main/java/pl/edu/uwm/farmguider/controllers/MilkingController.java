96ec4205419a3e0c3e5cb904010c99dbd1cd94c7
Adam <trentowski.adam@gmail.com>
2023-12-04 11:56:29 +0100

    Add MilkingFacade and MilkingController with create milking rest (and docs)

---
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/controllers/MilkingController.java
@@ -0,0 +1,63 @@
+package pl.edu.uwm.farmguider.controllers;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import pl.edu.uwm.farmguider.exceptions.ErrorResponse;
+import pl.edu.uwm.farmguider.facades.MilkingFacade;
+import pl.edu.uwm.farmguider.models.milking.dtos.MilkingCreateDTO;
+import pl.edu.uwm.farmguider.models.milking.dtos.MilkingResponseDTO;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/milking")
+@Tag(name = "Milking Controller", description = "Functionalities intended for milking.")
+public class MilkingController {
+
+    private final MilkingFacade milkingFacade;
+
+    @Operation(summary = "Create milking",
+            description = "Creates a cow's milking based on the provided payload")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Successful creation of milking",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = MilkingResponseDTO.class)
+                    )),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Bad Request - returns map of errors",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Not Found - cow not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    ))
+    })
+    @PostMapping("/create-milking/{cowId}")
+    @PreAuthorize("@fineGrainedAccessControl.compareGivenCowIdWithContext(#cowId)")
+    public ResponseEntity<MilkingResponseDTO> createMilking(@PathVariable Long cowId,
+                                                            @RequestBody @Valid MilkingCreateDTO milkingCreateDTO) {
+        MilkingResponseDTO milkingResponseDTO = milkingFacade.createMilking(cowId, milkingCreateDTO);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(milkingResponseDTO);
+    }
+
+}
---
+++ Backend/farmguider/src/main/java/pl/edu/uwm/farmguider/facades/MilkingFacade.java
@@ -0,0 +1,32 @@
+package pl.edu.uwm.farmguider.facades;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import pl.edu.uwm.farmguider.models.cow.Cow;
+import pl.edu.uwm.farmguider.models.milking.Milking;
+import pl.edu.uwm.farmguider.models.milking.dtos.MilkingCreateDTO;
+import pl.edu.uwm.farmguider.models.milking.dtos.MilkingResponseDTO;
+import pl.edu.uwm.farmguider.services.CowService;
+import pl.edu.uwm.farmguider.services.MilkingService;
+
+import static pl.edu.uwm.farmguider.models.milking.dtos.MilkingMapper.mapToMilkingResponseDTO;
+
+@Component
+@RequiredArgsConstructor
+public class MilkingFacade {
+
+    private final MilkingService milkingService;
+    private final CowService cowService;
+
+    public MilkingResponseDTO createMilking(Long cowId, MilkingCreateDTO milkingCreateDTO) {
+        Cow cow = cowService.getCowById(cowId);
+        Milking milking = milkingService.createMilking(
+                cow,
+                milkingCreateDTO.dateOfMilking(),
+                milkingCreateDTO.milkQuantity(),
+                milkingCreateDTO.milkingDuration()
+        );
+        return mapToMilkingResponseDTO(milking);
+    }
+
+}
