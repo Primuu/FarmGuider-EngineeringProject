6411b2e565311325177bcb0f1087f6b228c2ab72
Adam <trentowski.adam@gmail.com>
2023-12-19 08:33:03 +0100

    Add sql script to update fields table

---
+++ Backend/farmguider/src/main/resources/db/migration/V12__update_fields.sql
@@ -0,0 +1,3 @@
+ALTER TABLE public.fields ALTER COLUMN field_name SET NOT NULL;
+ALTER TABLE public.fields ALTER COLUMN soil_class TYPE VARCHAR(7);
+ALTER TABLE public.fields ALTER COLUMN soil_class SET NOT NULL;
