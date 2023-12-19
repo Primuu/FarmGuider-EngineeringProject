59afb2c57a7e6b6fb77d9ab8d08d81b73cfb7453
Adam <trentowski.adam@gmail.com>
2023-12-19 17:46:07 +0100

    Add fieldValidators and SoilClass enum

---
+++ Frontend/farmguider-app/src/entities/SoilClass.ts
@@ -0,0 +1,12 @@
+enum SoilClass {
+    I = "I",
+    II = "II",
+    IIIa = "III a",
+    IIIb = "III b",
+    IVa = "IV a",
+    IVb = "IV b",
+    V = "V",
+    Unknown = "Unknown"
+}
+
+export default SoilClass;
---
+++ Frontend/farmguider-app/src/utils/fieldValidators.ts
@@ -0,0 +1,35 @@
+import {TFunction} from "i18next";
+
+export type FieldValues = {
+    fieldName: string | null;
+    fieldArea: number | null;
+}
+
+export type FieldErrors = {
+    fieldName?: string;
+    fieldArea?: string;
+    soilClass?: string;
+}
+
+export const validateAddField = (values: FieldValues, t: TFunction): FieldErrors => {
+    const {fieldName, fieldArea} = values;
+    const tempErrors: FieldErrors = {};
+
+    tempErrors.fieldName = validateFieldName(fieldName, t);
+    tempErrors.fieldArea = validateFieldArea(fieldArea, t);
+
+    return tempErrors;
+}
+
+export const validateFieldName = (fieldName: string | null, t: TFunction): string => {
+    if (!fieldName) return t('addFieldModal.validation.fieldNameRequired');
+    if (fieldName && fieldName.length > 45) return t('addFieldModal.validation.fieldNameLength');
+    return '';
+};
+
+export const validateFieldArea = (fieldArea: number | null, t: TFunction): string => {
+    if (fieldArea === null) return t('addFieldModal.validation.fieldAreaRequired');
+    if (fieldArea < 0) return t('addFieldModal.validation.fieldAreaNonNegative');
+    if (fieldArea > 999.99) return t('addFieldModal.validation.fieldAreaRange');
+    return '';
+};
