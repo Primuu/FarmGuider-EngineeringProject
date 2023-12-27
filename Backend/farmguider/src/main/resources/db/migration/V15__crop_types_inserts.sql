6d774a5b5697ccfa649826f318fee6080a53844a
Adam <trentowski.adam@gmail.com>
2023-12-27 11:40:28 +0100

    Add sql script with crop types data insert

---
+++ Backend/farmguider/src/main/resources/db/migration/V15__crop_types_inserts.sql
@@ -0,0 +1,10 @@
+INSERT INTO crop_types (crop_type, optimal_planting_start_date, optimal_planting_end_date, optimal_harvest_start_date, optimal_harvest_end_date, average_yield) VALUES
+    ('WHEAT', '09-20', '10-10', '07-01', '07-31', 5.34),
+    ('TRITICALE', '09-10', '09-30', '07-15', '08-15', 7.35),
+    ('BARLEY', '09-15', '09-30', '06-25', '07-20', 5.92),
+    ('RAPESEED', '08-05', '08-31', '07-10', '07-31', 4.97),
+    ('RYE', '09-20', '10-05', '07-15', '08-15', 8.38),
+    ('OATS', '03-15', '04-10', '07-20', '08-10', 4.61),
+    ('CORN', '04-20', '05-20', '09-25', '10-20', 10.17),
+    ('POTATO', '03-15', '04-10', '08-20', '09-30', 29.413),
+    ('SUGAR_BEET', '03-15', '04-15', '09-20', '10-31', 63.12);
