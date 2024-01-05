62df15fe8b526cf1d1092a732bbf04e5b95e6ace
Adam <trentowski.adam@gmail.com>
2024-01-05 14:57:39 +0100

    Add calendarPage placeholder

---
+++ Frontend/farmguider-app/src/pages/CalendarPage/CalendarPage.tsx
@@ -0,0 +1,11 @@
+import '@/pages/CalendarPage/calendarPage.css';
+
+const CalendarPage = () => {
+    return (
+        <div className="calendar-container">
+            Calendar Placeholder
+        </div>
+    )
+}
+
+export default CalendarPage;
