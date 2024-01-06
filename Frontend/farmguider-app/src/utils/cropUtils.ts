a20330266aa646167fc766f5522d662d3f28590d
Adam <trentowski.adam@gmail.com>
2024-01-06 21:40:11 +0100

    Update crops table with fill color values

--- Frontend/farmguider-app/src/utils/cropUtils.ts
+++ Frontend/farmguider-app/src/utils/cropUtils.ts
@@ -2,17 +2,19 @@ import {FaWheatAwn} from "react-icons/fa6";
 import {LuWheat} from "react-icons/lu";
 import {GiBeet, GiCorn, GiFruitTree, GiPlantWatering, GiPotato} from "react-icons/gi";
 import {IconType} from "react-icons/lib";
+import {TbShovel} from "react-icons/tb";
 
 export const crops = [
-    {value: 'WHEAT', icon: LuWheat as IconType, labelKey: 'addCropModal.wheat'},
-    {value: 'TRITICALE', icon: FaWheatAwn as IconType, labelKey: 'addCropModal.triticale'},
-    {value: 'BARLEY', icon: FaWheatAwn as IconType, labelKey: 'addCropModal.barley'},
-    {value: 'RAPESEED', icon: GiFruitTree as IconType, labelKey: 'addCropModal.rapeseed'},
-    {value: 'RYE', icon: FaWheatAwn as IconType, labelKey: 'addCropModal.rye'},
-    {value: 'OATS', icon: GiPlantWatering as IconType, labelKey: 'addCropModal.oats'},
-    {value: 'CORN', icon: GiCorn as IconType, labelKey: 'addCropModal.corn'},
-    {value: 'POTATO', icon: GiPotato as IconType, labelKey: 'addCropModal.potato'},
-    {value: 'SUGAR_BEET', icon: GiBeet as IconType, labelKey: 'addCropModal.sugarBeet'},
+    {value: 'WHEAT', icon: LuWheat as IconType, labelKey: 'addCropModal.wheat', fill: '#F4E4A2'},
+    {value: 'TRITICALE', icon: FaWheatAwn as IconType, labelKey: 'addCropModal.triticale', fill: '#E5C29F'},
+    {value: 'BARLEY', icon: FaWheatAwn as IconType, labelKey: 'addCropModal.barley', fill: '#DBA159'},
+    {value: 'RAPESEED', icon: GiFruitTree as IconType, labelKey: 'addCropModal.rapeseed', fill: '#251203'},
+    {value: 'RYE', icon: FaWheatAwn as IconType, labelKey: 'addCropModal.rye', fill: '#C5B358'},
+    {value: 'OATS', icon: GiPlantWatering as IconType, labelKey: 'addCropModal.oats', fill: '#F3CB6A'},
+    {value: 'CORN', icon: GiCorn as IconType, labelKey: 'addCropModal.corn', fill: '#FFCC00'},
+    {value: 'POTATO', icon: GiPotato as IconType, labelKey: 'addCropModal.potato', fill: '#B59A6A'},
+    {value: 'SUGAR_BEET', icon: GiBeet as IconType, labelKey: 'addCropModal.sugarBeet', fill: '#D3A5BF'},
+    {value: 'NOT_PLANTED', icon: TbShovel as IconType, labelKey: 'addCropModal.notPlanted', fill: '#406064'},
 ];
 
 export const getCropIcon = (cropValue: string) => {
